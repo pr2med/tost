1a5426fa2776e5f0a0610ac97d72f142daa0301f
pr2med <denis100901@mail.ru>
2022-01-27 16:10:57 +0300

    New updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println ("Hello World");
+        byte num = 2;
+        System.out.println ("Skolko u menya yaic? ih " + 2);
+        float num_2 = 3.228f;
+        boolean happy = true;
+        String str = "Hello Anime World!^^";
+        System.out.println (str);
+        System.out.println ("A teper napishi skolko dash mne deneg");
+        Scanner numb = new Scanner (System.in);
+        int i;
+        i = numb.nextInt();
+        System.out.println ("Vsego lish " + i + "?" );
+
     }
 }
